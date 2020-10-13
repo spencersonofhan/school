4395cc8c723d5afd917217fa293e923f100070c8
Spencer Hanson <spencerbhans@gmail.com>
2020-10-13 16:18:35 -0600

    End of Git trouble, had to use --force. Online sources tell me that --force isn't good practice, espicially if others are using my remote repo... duly noted. Filled in evaluateStdVector() and evaluateStdArray(). Also created copyAndSortRaw() from the original copyAndSortRaw() to handle the raw array type sorting. The output looks good, still need to fix all warnings and run the gtest.

--- cpp/ArrayPerformance/sortutils.cpp
+++ cpp/ArrayPerformance/sortutils.cpp
@@ -2,6 +2,7 @@
 
 void printOutTimes(std::string, std::array<std::chrono::milliseconds, 5>);
 std::chrono::milliseconds copyAndSort(const SourceArray&);
+std::chrono::milliseconds copyAndSortRaw(const SourceArray&);
 
 void initializeRawArrayFromStdArray(const std::array<int, HOW_MANY_ELEMENTS>& source, int dest[])
 {
@@ -29,38 +30,66 @@ void evaluateRawArray(const SourceArray& random, const SourceArray& sorted, cons
     // Arrays to hold the SourceArray parameters and the total sort time for each array type
     std::array<std::chrono::milliseconds, 5> totalTimeArray;
 
-    totalTimeArray[0] = copyAndSort(random);
-    totalTimeArray[1] = copyAndSort(sorted);
-    totalTimeArray[2] = copyAndSort(reversed);
-    totalTimeArray[3] = copyAndSort(organPipe);
-    totalTimeArray[4] = copyAndSort(rotated);
+    totalTimeArray[0] = copyAndSortRaw(random);
+    totalTimeArray[1] = copyAndSortRaw(sorted);
+    totalTimeArray[2] = copyAndSortRaw(reversed);
+    totalTimeArray[3] = copyAndSortRaw(organPipe);
+    totalTimeArray[4] = copyAndSortRaw(rotated);
     printOutTimes("Raw Array", totalTimeArray);
 }
 
 void evaluateStdArray(const SourceArray& random, const SourceArray& sorted, const SourceArray& reversed, const SourceArray& organPipe, const SourceArray& rotated)
 {
+    std::array<std::chrono::milliseconds, 5> totalTimeArray;
 
+    totalTimeArray[0] = copyAndSort(random);
+    totalTimeArray[1] = copyAndSort(sorted);
+    totalTimeArray[2] = copyAndSort(reversed);
+    totalTimeArray[3] = copyAndSort(organPipe);
+    totalTimeArray[4] = copyAndSort(rotated);
+    printOutTimes("std::array", totalTimeArray);
 }
 
 void evaluateStdVector(const SourceArray& random, const SourceArray& sorted, const SourceArray& reversed, const SourceArray& organPipe, const SourceArray& rotated)
 {
+    std::array<std::chrono::milliseconds, 5> totalTimeArray;
 
+    totalTimeArray[0] = copyAndSort(random);
+    totalTimeArray[1] = copyAndSort(sorted);
+    totalTimeArray[2] = copyAndSort(reversed);
+    totalTimeArray[3] = copyAndSort(organPipe);
+    totalTimeArray[4] = copyAndSort(rotated);
+    printOutTimes("std::vector", totalTimeArray);
 }
 
 void printOutTimes(std::string name, std::array<std::chrono::milliseconds, 5> data)
 {
-    std::cout << " --- " << name << " Performance ---" << std::endl;
+    std::cout << "\n --- " << name << " Performance ---" << std::endl;
     for (uint32_t i = 0; i < data.max_size(); i++)
     {
         std::cout << std::setw(PRINTOUT_WIDTH) << std::left << PRE_SORT_NAMES[i]
-        << " : " << data[i].count() << " ms\n" << std::endl;
+        << " : " << data[i].count() << " ms" << std::endl;
     }
 }
 
 std::chrono::milliseconds copyAndSort(const SourceArray& arrayCopy)
 {
     std::chrono::milliseconds totalTime(0);
-    std::cout << totalTime.count() << std::endl;
+    for (std::uint32_t j = 0; j < HOW_MANY_TIMES; j++)
+    {
+        std::array<int, HOW_MANY_ELEMENTS> theCopy = arrayCopy;
+
+        auto startTime = std::chrono::steady_clock::now();
+        std::sort(std::begin(theCopy), std::end(theCopy));
+        auto endTime = std::chrono::steady_clock::now();
+        totalTime += std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+    }
+    return totalTime;
+}
+
+std::chrono::milliseconds copyAndSortRaw(const SourceArray& arrayCopy)
+{
+    std::chrono::milliseconds totalTime(0);
     for (std::uint32_t j = 0; j < HOW_MANY_TIMES; j++)
     {
         int raw[HOW_MANY_ELEMENTS];
